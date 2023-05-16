690ea7cdd4582a3ef932c7daaa64bdb85c6a2bea
mego <megocapps@gmail.com>
2023-05-15 21:57:58 -0400

    project update

---
+++ Promises/pokemon_api/app.js
@@ -0,0 +1,59 @@
+let pokemonData = await Promise.all(
+    randomPokemonUrls.map(url => $.getJSON(url))
+  );
+  let speciesData = await Promise.all(
+    pokemonData.map(p => $.getJSON(p.species.url))
+  );
+  descriptions = speciesData.map(d => {
+    let descriptionObj = d.flavor_text_entries.find(
+      entry => entry.language.name === "en"
+    );
+    return descriptionObj
+      ? descriptionObj.flavor_text
+      : "No description available.";
+  });
+  descriptions.forEach((desc, i) => {
+    console.log(`${pokemonData[i].name}: ${desc}`);
+  });
+}
+
+// 4.
+let $btn = $("button");
+let $pokeArea = $("#pokemon-area");
+
+$btn.on("click", async function() {
+  $pokeArea.empty();
+  let allData = await $.getJSON(`${baseURL}/pokemon/?limit=1000`);
+  let randomPokemonUrls = [];
+  for (let i = 0; i < 3; i++) {
+    let randomIdx = Math.floor(Math.random() * allData.results.length);
+    let url = allData.results.splice(randomIdx, 1)[0].url;
+    randomPokemonUrls.push(url);
+  }
+  let pokemonData = await Promise.all(
+    randomPokemonUrls.map(url => $.getJSON(url))
+  );
+  let speciesData = await Promise.all(
+    pokemonData.map(p => $.getJSON(p.species.url))
+  );
+  speciesData.forEach((d, i) => {
+    let descriptionObj = d.flavor_text_entries.find(function(entry) {
+      return entry.language.name === "en";
+    });
+    let description = descriptionObj ? descriptionObj.flavor_text : "";
+    let name = pokemonData[i].name;
+    let imgSrc = pokemonData[i].sprites.front_default;
+    $pokeArea.append(makePokeCard(name, imgSrc, description));
+  });
+});
+
+function makePokeCard(name, imgSrc, description) {
+  return `
+    <div class="card">
+      <h1>${name}</h1>
+      <img src=${imgSrc} />
+      <p>${description}</p>
+    </div>
+  `;
+}
+});
